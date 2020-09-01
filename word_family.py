5276a0163975b5ca8683fc92c6a3dc053c594697
Alexandra Ciobotaru <Alegzandra@users.noreply.github.com>
2020-09-01 19:28:49 +0300

    Update word_family.py

--- word_family.py
+++ word_family.py
@@ -1,44 +1,90 @@
-#from nltk.stem.porter import *   # I will use snowball stemmer
 import nltk
 from nltk.stem import SnowballStemmer
 from collections import defaultdict
 
-word_list = []
-'''
-with open("Lista cuvinte romana a-z.txt", encoding = 'utf-8') as fp:
-    line = fp.readline()
-    cnt = 1
-    while line:
-        #print("Line {}: {}".format(cnt, line.strip()))
-        word_list.append(line.strip())
-        line = fp.readline()
-        cnt += 1
-fp.close()
-'''
-with open("Lista cuvinte romana a-z.txt") as fp:
-    for line in enumerate(fp):
-        word_list.append(line)
 
-print(word_list)
+def generate_words_from_stem(input_word):
+    word_list = []
+    with open("lista_cuvinte.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            word_list.append(line[1].replace('\n', ''))
+    vocab = set(word_list)
+    stemmer = SnowballStemmer("romanian")
+    d = defaultdict(set)
+    for v in vocab:
+        d[stemmer.stem(v)].add(v)
 
+    input_word = stemmer.stem(input_word)
 
-vocab = set(word_list)
-print(vocab)
-#vocab = set(['grab', 'grabbing', 'grabbed', 'run', 'running', 'eat'])
-#vocab = set(['apuca', 'apucând', 'apucat', 'alerga', 'alergând', 'mânca'])
+    return print(d[input_word])
 
-# Here porter stemmer, but can be any other stemmer too
-stemmer = SnowballStemmer("romanian")
+def generate_words_with_prefixes(input_word):
+    word_list = []
+    with open("lista_cuvinte.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            word_list.append(line[1].replace('\n', ''))
+    prefixes_list = []
+    with open("lista_prefixe.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            prefixes_list.append(line[1].replace('\n', ''))
+    new_word_list = []
+    good_words = []
+    for prefix in prefixes_list:
+        new_word_list.append(prefix+input_word)
+    for word in new_word_list:
+        if word in word_list:
+            good_words.append(word)
+    return print(good_words)
 
-d = defaultdict(set)
-for v in vocab:
-    d[stemmer.stem(v)].add(v)  
+def generate_words_with_sufixes(input_word):
+    word_list = []
+    with open("lista_cuvinte.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            word_list.append(line[1].replace('\n', ''))
+    sufixes_list = []
+    with open("lista_sufixe.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            sufixes_list.append(line[1].replace('\n', ''))
+    new_word_list = []
+    good_words = []
+    for sufix in sufixes_list:
+        new_word_list.append(input_word+sufix)
+    for word in new_word_list:
+        if word in word_list:
+            good_words.append(word)
+    return print(good_words)
 
-#print(d)
-# defaultdict(<class 'set'>, {'grab': {'grab', 'grabbing', 'grabbed'}, 'eat': {'eat'}, 'run': {'run', 'running'}}
-# Now we have a dictionary that maps stems to the valid words that can generate them. For any stem we can do the following:
+def generate_words_with_sufixes_and_prefixes(input_word):
+    word_list = []
+    with open("lista_cuvinte.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            word_list.append(line[1].replace('\n', ''))
+    sufixes_list = []
+    with open("lista_sufixe.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            sufixes_list.append(line[1].replace('\n', ''))
+    prefixes_list = []
+    with open("lista_prefixe.txt", encoding='utf-8') as fp:
+        for line in enumerate(fp):
+            prefixes_list.append(line[1].replace('\n', ''))
 
-print(d['apuc'])
-# {'grab', 'grabbed', 'grabbing'}
+    new_list = []
+    new_list2 = []
+    for prefix in prefixes_list:
+        new_list.append(prefix+input_word)
+    for sufix in sufixes_list:
+        for word in new_list:
+            new_list2.append(word+sufix)
+    #facem verificarea ca exista
+    good_words = []
+    for word in new_list2:
+        if word in word_list:
+            good_words.append(word)
 
-#For building the vocabulary you can tokenize a corpus or use nltk's built-in dictionary of English words.
+    return print(good_words)
+
+#generate_words_from_stem(input("Tastați cuvântul dorit: "))
+#generate_words_with_prefixes('calcul')
+#generate_words_with_sufixes('calcul')
+generate_words_with_sufixes_and_prefixes('teroare')
+#generate_words_from_stem('calcul') #nu merge foarte bine
